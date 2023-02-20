82cc300b5574d7e13603946838542907f3d55705
Harsha Murupudi <itsmeeeharsha@gmail.com>
2023-02-20 08:56:58 +0530

    feat(WorldModel): Make model full screen

--- client/src/features/misc/routes/Landing.jsx
+++ client/src/features/misc/routes/Landing.jsx
@@ -9,11 +9,13 @@ export const Landing = () => {
   const navigate = useNavigate();
 
   return (
-    <Canvas shadows camera={{ position: [0, 0, 1.66] }}>
-      <Environment preset='forest' />
-      <World />
-      <ContactShadows position={[0, -0.8, 0]} color='#ffffff' />
-      <OrbitControls autoRotate />
-    </Canvas>
+    <div style={{ width: '100vw', height: '100vh' }}>
+      <Canvas shadows camera={{ position: [0, 0, 1.66] }}>
+        <Environment preset='forest' />
+        <World />
+        <ContactShadows position={[0, -0.8, 0]} color='#ffffff' />
+        <OrbitControls autoRotate />
+      </Canvas>
+    </div>
   );
 };
