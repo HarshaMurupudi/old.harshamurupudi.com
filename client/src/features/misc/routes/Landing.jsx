9267020c02ff5fe444bd10e2a3a7e5cb4d185315
Harsha Murupudi <itsmeeeharsha@gmail.com>
2023-02-24 21:22:29 +0530

    feat(WorldModel): add camera helper - inital version

--- client/src/features/misc/routes/Landing.jsx
+++ client/src/features/misc/routes/Landing.jsx
@@ -1,9 +1,44 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { useNavigate } from 'react-router';
 import { OrbitControls, Environment, ContactShadows } from '@react-three/drei';
-import { Canvas } from '@react-three/fiber';
+import { Canvas, useFrame, useThree } from '@react-three/fiber';
+import { DoubleSide, Mesh, PerspectiveCamera } from 'three';
 
 import { World } from '../../../components/models';
+
+function Box() {
+  const boxRef = useRef(null);
+
+  useFrame(() => {
+    boxRef.current.rotation.x += 0.005;
+    boxRef.current.rotation.y += 0.01;
+  });
+
+  return (
+    <mesh ref={boxRef}>
+      <boxGeometry args={[1, 1, 1]} />
+      <meshStandardMaterial color='orange' side={DoubleSide} />
+    </mesh>
+  );
+}
+
+function Controls() {
+  const {
+    camera,
+    gl: { domElement },
+  } = useThree();
+
+  return <OrbitControls args={[camera, domElement]} />;
+}
+
+function CameraHelper() {
+  const camera = new PerspectiveCamera(60, 1, 1, 3);
+  return (
+    <group position={[0, 0, 2]}>
+      <cameraHelper args={[camera]} />
+    </group>
+  );
+}
 
 export const Landing = () => {
   const navigate = useNavigate();
@@ -14,6 +49,9 @@ export const Landing = () => {
         shadows
         camera={{ position: [-70, 30, 100], fov: 10, near: 0.1, far: 1000 }}
       >
+        <Controls />
+        <Box />
+        <CameraHelper />
         <Environment preset='forest' />
         <World />
         <ContactShadows position={[0, -0.8, 0]} color='#ffffff' />
