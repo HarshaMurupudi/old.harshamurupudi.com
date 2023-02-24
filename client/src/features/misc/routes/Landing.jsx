8b361f1bf358df23113b0e44dbcdd95f301c398b
Harsha Murupudi <itsmeeeharsha@gmail.com>
2023-02-24 09:13:36 +0530

    feat(WorldModel): update zoom to make entire scene visbile - final - before camera helper

--- client/src/features/misc/routes/Landing.jsx
+++ client/src/features/misc/routes/Landing.jsx
@@ -10,7 +10,10 @@ export const Landing = () => {
 
   return (
     <div style={{ width: '100vw', height: '100vh' }}>
-      <Canvas shadows camera={{ position: [-80, 28, 100], fov: 20 }}>
+      <Canvas
+        shadows
+        camera={{ position: [-70, 30, 100], fov: 10, near: 0.1, far: 1000 }}
+      >
         <Environment preset='forest' />
         <World />
         <ContactShadows position={[0, -0.8, 0]} color='#ffffff' />
